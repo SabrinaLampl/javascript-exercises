71642f46886ec9b0e68f0f81651ef7adb7411e94
SabrinaLampl <lampl.sabrina@gmail.com>
2022-12-12 17:20:24 +0100

    Implemented removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function() {
+const removeOneFromArray = function (arr, value) {
+  let array = arr;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      array.splice(i, 1);
+    }
+  }
 
+  return array;
 };
 
+
+
+const removeFromArray = function (arr, value, ...manyMoreArgs) {
+  let array = removeOneFromArray(arr, value);
+  for (const item of manyMoreArgs) {
+    array = removeOneFromArray(array, item);
+  }
+
+  return array;
+};
+console.log(removeFromArray([1, 2, 3, 4], 3, 2))
 // Do not edit below this line
 module.exports = removeFromArray;
