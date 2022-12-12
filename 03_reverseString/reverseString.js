b52128fa8a51d0a06aba129ddddaf74e69df710e
SabrinaLampl <lampl.sabrina@gmail.com>
2022-12-12 15:45:03 +0100

    Implemented reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+const splitString = string.split('');
+const reverseSplitString = splitString.reverse ();
+const joinReverseSplitString = reverseSplitString.join('');
+return joinReverseSplitString;
 };
+
+console.log (reverseString('hello there'))
 
 // Do not edit below this line
 module.exports = reverseString;
