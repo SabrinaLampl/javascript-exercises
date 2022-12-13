efaed3de725e20627a05c40c5240c3a96c27ff5b
SabrinaLampl <lampl.sabrina@gmail.com>
2022-12-13 12:28:45 +0100

    Implemented tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (x) {
+  const celsius = (x - 32) * (5 / 9);
+  const roundedCelsius = Math.round(celsius * 10) / 10;
+  return roundedCelsius
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (x) {
+  const fahrenheit = (x * (9 / 5) + 32);
+  const roundedFahrenheit = Math.round(fahrenheit * 10) / 10;
+  return roundedFahrenheit;
+
 };
+
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
